20da522e4ba0e6518dbc8913651b434a12fa6c6b
rishavdash <riishav@ineuron.ai>
2022-11-02 19:46:14 +0530

    added aws credentials

--- xray/configuration/s3_operations.py
+++ xray/configuration/s3_operations.py
@@ -7,6 +7,7 @@ from botocore.exceptions import ClientError
 import boto3
 from xray.exception import XrayException
 from mypy_boto3_s3.service_resource import Bucket
+from xray.constants import *
 
 
 
@@ -14,11 +15,32 @@ import logging
 MODEL_SAVE_FORMAT = ".pt"
 
 class S3Operation:
+    s3_client=None
+    s3_resource = None
     def __init__(self):
-        self.s3_client = boto3.client("s3")
+        # self.s3_client = boto3.client("s3")
 
-        self.s3_resource = boto3.resource("s3")
-
+        # self.s3_resource = boto3.resource("s3")
+        if S3Operation.s3_resource==None or S3Operation.s3_client==None:
+            __access_key_id = os.getenv(AWS_ACCESS_KEY_ID_ENV_KEY, )
+            __secret_access_key = os.getenv(AWS_SECRET_ACCESS_KEY_ENV_KEY, )
+            if __access_key_id is None:
+                raise Exception(f"Environment variable: {AWS_ACCESS_KEY_ID_ENV_KEY} is not not set.")
+            if __secret_access_key is None:
+                raise Exception(f"Environment variable: {AWS_SECRET_ACCESS_KEY_ENV_KEY} is not set.")
+        
+            S3Operation.s3_resource = boto3.resource('s3',
+                                            aws_access_key_id=__access_key_id,
+                                            aws_secret_access_key=__secret_access_key,
+                                            region_name=REGION_NAME
+                                            )
+            S3Operation.s3_client = boto3.client('s3',
+                                        aws_access_key_id=__access_key_id,
+                                        aws_secret_access_key=__secret_access_key,
+                                        region_name=REGION_NAME
+                                        )
+        self.s3_resource = S3Operation.s3_resource
+        self.s3_client = S3Operation.s3_client
     @staticmethod
     def read_object(
         object_name: str, decode: bool = True, make_readable: bool = False
